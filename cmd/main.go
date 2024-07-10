a165f7cde4bb8420293e58ed8971a11ab16c6327
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-07-10 16:31:44 +0500

    feat: create tables & load config

--- cmd/main.go
+++ cmd/main.go
@@ -5,18 +5,28 @@ import (
 	"github.com/TauAdam/todo-list/pkg/handler"
 	"github.com/TauAdam/todo-list/pkg/repository"
 	"github.com/TauAdam/todo-list/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("failed to init config: %v", err)
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	server := new(todolist.Server)
-	port := "8080"
+	port := viper.GetString("port")
 	if err := server.Run(port, handlers.InitRoutes()); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 
+}
+
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
