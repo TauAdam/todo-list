1df4fb54c2ee2102e0c2df6205a2cd7d9d0b6d67
TauAdam <101412464+TauAdam@users.noreply.github.com>
2024-07-11 22:24:45 +0500

    feat: sign up user

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -9,9 +9,18 @@ import (
 func (h *Handler) sighUp(c *gin.Context) {
 	var input todolist.User
 	if err := c.BindJSON(&input); err != nil {
-		NewError(c, http.StatusBadRequest, err.Error())
+		NewErrorResponse(c, http.StatusBadRequest, err.Error())
 		return
 	}
+	id, err := h.services.Auth.CreateUser(input)
+	if err != nil {
+		NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+
+	}
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 func (h *Handler) sighIn(c *gin.Context) {
 
--- pkg/repository/auth_postgres.go
+++ pkg/repository/auth_postgres.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"fmt"
 	todolist "github.com/TauAdam/todo-list"
 	"github.com/jmoiron/sqlx"
 )
@@ -13,5 +14,11 @@ func NewAuthPostgres(db *sqlx.DB) *AuthPostgres {
 	return &AuthPostgres{db: db}
 }
 func (r *AuthPostgres) CreateUser(user todolist.User) (int, error) {
-	return 0, nil
+	query := fmt.Sprintf("INSERT INTO %s (name, username, password) values ($1, $2, $3) RETURNING id", usersTable)
+	row := r.db.QueryRow(query, user.Name, user.Username, user.Password)
+	var id int
+	if err := row.Scan(&id); err != nil {
+		return 0, err
+	}
+	return id, nil
 }
